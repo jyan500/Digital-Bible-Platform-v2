f087bd2faad953aedefe21f63e8916d19f5cd075
Jansen Yan <jyan500@gmail.com>
2018-11-01 21:00:23 -0700

    Extracted sidebar html, css and js into its own files and customized css with dark purple and light blue themes

---
+++ static/sidebar.js
@@ -0,0 +1,25 @@
+// SIDENAV
+$(document).ready(function(){
+	$('.show-sidebar').click(function(){
+		$('.sidebar').show();
+		$('.sidebar-close').show();
+	});
+
+	$('.close-sidebar').click(function(){
+		$('.sidebar').hide();
+	});
+
+	// when user closes the sidebar in the mobile view, but
+	// resizes to full width, then show the sidebar
+	document.getElementsByTagName('body')[0].onresize = function (){
+		console.log("resized");
+		if (window.matchMedia("(max-width: 800px)").matches){
+			console.log("less than 800px");
+			$('.sidebar').hide();
+		}
+		else{
+			console.log("greater than 800px");
+			$('.sidebar').show();
+		}
+	};
+})
