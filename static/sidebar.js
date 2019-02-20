cd954bd5def19057e8d8e9d7479736f0dabe0cb9
Jansen Yan <jyan500@gmail.com>
2019-02-19 20:25:42 -0800

    dbt v2 fixed mobile view, added mobile sidebar

--- static/sidebar.js
+++ static/sidebar.js
@@ -1,31 +1,26 @@
 // SIDENAV
 $(document).ready(function(){
-	$('.show-sidebar').click(function(){
-		$('.sidebar').show();
-		$('.sidebar-close').show();
-	});
-
-	$('.close-sidebar').click(function(){
-		$('.sidebar').hide();
-	});
-
-	// when user closes the sidebar in the mobile view, but
-	// resizes to full width, then show the sidebar
-	document.getElementsByTagName('body')[0].onresize = function (){
-		console.log("resized");
-		if (window.matchMedia("(max-width: 800px)").matches){
-			console.log("less than 800px");
-			$('.sidebar').hide();
-		}
-		else{
-			console.log("greater than 800px");
-			$('.sidebar').show();
-		}
-	};
 	// adds active class to the current link 
     $('a').each(function(){
         if ($(this).prop('href') == window.location.href) {
             $(this).addClass('active'); $(this).parents('li').addClass('active');
         }
     });
+	$('.show-sidebar').click(function(){
+		$('.mobile-sidebar').toggle(300);
+	});
+	// when user closes the sidebar in the mobile view, but
+	// resizes to full width, then show the sidebar
+	document.getElementsByTagName('body')[0].onresize = function (){
+		console.log("resized");
+		if (window.matchMedia("(max-width: 800px)").matches){
+			console.log("less than 800px");
+			$('.mobile-sidebar').hide();
+		}
+		else{
+			console.log("greater than 800px");
+			$('.sidebar').show();
+		}
+	};
+	
 })
