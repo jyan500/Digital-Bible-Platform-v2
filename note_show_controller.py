bc379bdcea35ec17d0de94b3db9b6605c7ada17b
Jansen Yan <jyan500@gmail.com>
2019-03-06 16:18:51 -0800

    fixed bug redirect to login page was not occuring for the notes page if user wasn't logged in

--- note_show_controller.py
+++ note_show_controller.py
@@ -4,6 +4,9 @@ from extensions import *
 note_show_controller = Blueprint('note_show_controller', __name__)
 @note_show_controller.route('/note_show_controller', methods = ["GET", "POST"])
 def note_show_page():
+	## users should be prompted to login before going to the index page 
+	if (not isUserLoggedIn()):
+		return redirect(url_for('login.loginpage'))
 	cur = mysql.connection.cursor()
 	user_id = getUserID(cur, session['username'])
 	results_assoc = getExistingNotes(cur, user_id)
