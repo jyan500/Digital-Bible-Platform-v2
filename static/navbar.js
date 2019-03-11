b76872e34d7e30777490be657d747cca69688375
Jansen Yan <jyan500@gmail.com>
2019-03-10 23:52:12 -0700

    changed layout to the navbar top style instead of sidebar

---
+++ static/navbar.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	// $('.nav-item').hover(
+	// 	// on Enter
+	// 	function(){
+	// 		$(this).find('a').append("<div class = 'border'></div>");	
+	// 		$('.border').css('display', 'block');
+	// 		$('.border').width($(this).width());
+	// 	},
+	// 	// on Leave
+	// 	function(){
+	// 		$('.border').remove();
+	// 	});
+
+});
--- static/sidebar.js
+++ static/sidebar.js
@@ -11,20 +11,20 @@ $(document).ready(function(){
 	});
 	// when user closes the sidebar in the mobile view, but
 	// resizes to full width, then show the sidebar
-	document.getElementsByTagName('body')[0].onresize = function (){
-		console.log("resized");
-		if (window.matchMedia("(max-width: 800px)").matches){
-			// console.log("less than 800px");
-			$('.mobile-sidebar').hide();
-			$('.hamburger').show();
-			$('.sidebar').hide();
-		}
-		else{
-			// console.log("greater than 800px");
-			$('.sidebar').show();
-			$('.mobile-sidebar').hide();
-			$('.hamburger').hide();
-		}
-	};
+	// document.getElementsByTagName('body')[0].onresize = function (){
+	// 	console.log("resized");
+	// 	if (window.matchMedia("(max-width: 800px)").matches){
+	// 		console.log("less than 800px");
+	// 		$('.mobile-sidebar').hide();
+	// 		$('.hamburger').show();
+	// 		$('.sidebar').hide();
+	// 	}
+	// 	else{
+	// 		console.log("greater than 800px");
+	// 		$('.sidebar').show();
+	// 		$('.mobile-sidebar').hide();
+	// 		$('.hamburger').hide();
+	// 	}
+	// };
 	
 })
